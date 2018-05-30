d2fd45a7e8b6785d56b4296eff4491e031f223ea
程康 <chengkang@rongzi.com>
2018-05-30 10:33:43 +0800

    finish

---
+++ src/pages/Login.vue
@@ -0,0 +1,51 @@
+<template>
+<div>
+    <input type="text" placeholder="用户名" v-model="userName">
+    <input type="text" placeholder="密码" v-model="password">
+    <button @click="login">登录</button>
+</div>
+</template>
+
+<script>
+import MintUI from 'mint-ui'
+export default {
+    data() {
+        return {
+            userName:"",
+            password:""
+        }
+    },
+
+    methods:{
+        login:function(){
+            console.log('loginAciton');
+            let url = "http://localhost:8079/LoginServlet";
+            MintUI.Indicator.open({text: '加载中...',spinnerType: 'triple-bounce'})
+            this.$http.get(url,{
+                // axios.get(url,{
+                params:{
+                    "username":this.userName,
+                    "password":this.password   
+                }
+            }).then((responseData)=>{
+                console.log(responseData);
+                var ret = responseData.data.ret;
+                if(0 == ret){
+                    console.log("登录成功");
+                }else{
+                    console.log("登录失败");
+                }
+                MintUI.Indicator.close();
+            },(response)=>{
+                MintUI.Indicator.close();
+                console.log(response);
+            })
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+
+   
+</style>
